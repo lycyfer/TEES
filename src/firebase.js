47c48964555442a3f558559a98d22ec669783cad
Vitalik <82965231+lycyfer@users.noreply.github.com>
2024-04-06 20:12:10 +0300

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -6,16 +6,7 @@ import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 import { getDatabase } from 'firebase/database';
 
-const firebaseConfig = {
-    apiKey: "AIzaSyC6erI31TkvwUH4qm54BV6t2Anu02vO8Pg",
-    authDomain: "twilight-10eff.firebaseapp.com",
-    databaseURL: "https://twilight-10eff-default-rtdb.asia-southeast1.firebasedatabase.app",
-    projectId: "twilight-10eff",
-    storageBucket: "twilight-10eff.appspot.com",
-    messagingSenderId: "233447691500",
-    appId: "1:233447691500:web:8b98efa0fa6d4cb2e681c9",
-    measurementId: "G-9NTKMMZZXN"
-};
+
 
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
